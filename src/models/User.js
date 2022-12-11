5a1c5fc5d58ff5fa270fa9c5325a0eb6bc5b45a0
unknown <rafhaelgomes.dev>
2022-12-10 21:07:11 -0300

    requisito 2

--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,7 @@
 
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
