0d50cd50fae528fdb23b124016e6cde0e1871d14
unknown <rafhaelgomes.dev>
2022-12-10 21:03:32 -0300

    Requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,16 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    underscored: true,
+    tableName: 'users',
+    timestamps: false,
+  });
+
+  return User;
+};
+
+module.exports = User;
