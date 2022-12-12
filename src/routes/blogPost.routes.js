47f96b4fbd2b94f8f7a4686338626a1b4bb9898d
unknown <rafhaelgomes.dev>
2022-12-11 22:20:00 -0300

    requisito 11

--- src/routes/blogPost.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const validateToken = require('../middlewares/validateToken');
-
-router.post('/', validateToken, create);
-
-module.exports = router;
--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ const app = require('./app');
 const loginRouter = require('./routes/login.routes');
 const userRouter = require('./routes/user.routes');
 const categoriesRouter = require('./routes/categories.routes');
-const blogPostRouter = require('./routes/blogPost.routes');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -17,6 +16,5 @@ app.get('/', (_request, response) => {
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
 app.use('/categories', categoriesRouter);
-app.use('/post', blogPostRouter);
 
 app.listen(port, () => console.log('ouvindo porta', port));
