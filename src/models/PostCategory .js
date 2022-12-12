99418001af09fcedb0a4e33f402ce1f4f0a9f3ad
unknown <rafhaelgomes.dev>
2022-12-11 22:07:21 -0300

    Requisito 11

---
+++ src/models/PostCategory .js
@@ -0,0 +1,35 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+  }, {
+    underscored: true,
+    tableName: 'posts_categories',
+    timestamps: false,
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
